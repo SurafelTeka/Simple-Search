7a15983e242e8717d4018454022e55b4c69b9fac
Surafel Teka <se.surafel.teka@gmail.com>
2024-04-21 23:45:17 +0300

    card-list

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 
+import "./card-list.styles.css";
 class CardList extends Component {
   render() {
     console.log(this.props.monsters);
@@ -7,10 +8,20 @@ class CardList extends Component {
     const { monsters } = this.props;
 
     return (
-      <div>
-        {monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+      <div className="card-list">
+        {monsters.map((monster) => {
+          const { name, id, email } = monster;
+          return (
+            <div className="card-container" key={monster.id}>
+              <img
+                alt={`monster ${name}`}
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+              />
+              <h2>{name}</h2>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
