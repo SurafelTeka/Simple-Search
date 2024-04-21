9e8ab86c450b3bf7937fbd96699580cfe6ea7717
Surafel Teka <se.surafel.teka@gmail.com>
2024-04-21 22:58:46 +0300

    search component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -51,13 +52,11 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="search monsters"
-          onChange={onSearchChange}
+        <SearchBox
+          className={"search-box"}
+          onChangeHandler={onSearchChange}
+          placeholder={"search monsters"}
         />
-
         {/* {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
